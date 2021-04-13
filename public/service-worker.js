09f75844179349ca4c1675123fa080762461e794
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-12 21:04:21 -0400

    add service-worker link

---
+++ public/service-worker.js
@@ -0,0 +1,57 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/db.js',
+    '/styles.css',
+    '/index.js',
+    '/index.html',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+  ];
+  
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  self.addEventListener("install", function(e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Successfully cached!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", function(e) {
+    e.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removed cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", function(e) {
+    e.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(e.request).then(response => {
+          return response || fetch(e.request);
+        });
+      })
+    );
+  });
+  
+
+self.onsync = function(e) {
+    if(e.tag == 'db-sync') {
+        e.waitUntil(sendToServer());
+    }
+}
